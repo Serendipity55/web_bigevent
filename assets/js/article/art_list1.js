6703a443ffa5e6f18c52d67b1024694a2eeddb8f
Serendipity_whl <rachel.simopicture@gmail.com>
2021-12-30 20:38:04 +0800

    完成文章管理功能的开发

--- assets/js/article/art_list1.js
+++ assets/js/article/art_list1.js
@@ -3,26 +3,25 @@ $(function() {
     var form = layui.form
     var laypage = layui.laypage
 
+
     // 定义美化时间的过滤器
     template.defaults.imports.dataFormat = function(date) {
         const dt = new Date(date)
 
-        var y = dt.getFullYear()
-        var m = padZero(dt.getMonth() + 1)
-        var d = padZero(dt.getDate())
+        var y = dt.getFullYear();
+        var m = padZero(dt.getMonth() + 1);
+        var d = padZero(dt.getDate());
 
-        var hh = padZero(dt.getHours())
-        var mm = padZero(dt.getMinutes())
-        var ss = padZero(dt.getSeconds())
-
+        var hh = padZero(dt.getHours());
+        var mm = padZero(dt.getMinutes());
+        var ss = padZero(dt.getSeconds());
         return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
     }
 
-    // 定义补零的函数
+    // 定义补零函数
     function padZero(n) {
         return n > 9 ? n : '0' + n
     }
-
     // 定义一个查询的参数对象，将来请求数据的时候，
     // 需要将请求参数对象提交到服务器
     var q = {
@@ -38,54 +37,56 @@ $(function() {
     // 获取文章列表数据的方法
     function initTable() {
         $.ajax({
-            method: 'GET',
-            url: '/my/article/list',
-            data: q,
+            contentType: 'application/json;charset=UTF-8',
+            type: 'POST',
+            url: 'http://mm.bigbro.fun/task/whl/my/article/list',
+            data: JSON.stringify(q),
+            dataType: "json",
             success: function(res) {
+                console.log(res);
                 if (res.status !== 0) {
                     return layer.msg('获取文章列表失败！')
                 }
                 // 使用模板引擎渲染页面的数据
-                var htmlStr = template('tpl-table', res)
-                $('tbody').html(htmlStr)
-                    // 调用渲染分页的方法
-                renderPage(res.total)
+                var htmlStr = template('tpl-table', res);
+                $('tbody').html(htmlStr);
+                // 调用渲染分页的方法
+                renderPage(res.data.total)
             }
         })
     }
-
     // 初始化文章分类的方法
     function initCate() {
         $.ajax({
-            method: 'GET',
-            url: '/my/article/cates',
+            method: 'POST',
+            url: 'http://mm.bigbro.fun/task/whl/my/article/cates',
             success: function(res) {
                 if (res.status !== 0) {
-                    return layer.msg('获取分类数据失败！')
+                    return layer.msg('获取分类数据失败')
                 }
                 // 调用模板引擎渲染分类的可选项
-                var htmlStr = template('tpl-cate', res)
+                var htmlStr = template('tpl-cate', res);
+                // 利用属性选择器选中name属性
                 $('[name=cate_id]').html(htmlStr)
                     // 通过 layui 重新渲染表单区域的UI结构
                 form.render()
+
             }
         })
     }
-
     // 为筛选表单绑定 submit 事件
     $('#form-search').on('submit', function(e) {
-        e.preventDefault()
+            e.preventDefault();
             // 获取表单中选中项的值
-        var cate_id = $('[name=cate_id]').val()
-        var state = $('[name=state]').val()
-            // 为查询参数对象 q 中对应的属性赋值
-        q.cate_id = cate_id
-        q.state = state
-            // 根据最新的筛选条件，重新渲染表格的数据
-        initTable()
-    })
-
-    // 定义渲染分页的方法
+            var cate_id = $('[name=cate_id]').val();
+            var state = $('[name=state]').val();
+            // 为查询参数对象q中对应的属性赋值
+            q.cate_id = cate_id;
+            q.state = state
+                // 根据最新的筛选条件，重新渲染表格的数据
+            initTable();
+        })
+        // 定义渲染分页的方法
     function renderPage(total) {
         // 调用 laypage.render() 方法来渲染分页的结构
         laypage.render({
@@ -117,19 +118,18 @@ $(function() {
             }
         })
     }
-
     // 通过代理的形式，为删除按钮绑定点击事件处理函数
     $('tbody').on('click', '.btn-delete', function() {
         // 获取删除按钮的个数
         var len = $('.btn-delete').length
-        console.log(len)
-            // 获取到文章的 id
+        console.log(len);
+        // 获取到文章的 id
         var id = $(this).attr('data-id')
             // 询问用户是否要删除数据
         layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
             $.ajax({
                 method: 'GET',
-                url: '/my/article/delete/' + id,
+                url: 'http://mm.bigbro.fun/task/whl/my/article/delete/' + id,
                 success: function(res) {
                     if (res.status !== 0) {
                         return layer.msg('删除文章失败！')
@@ -147,7 +147,6 @@ $(function() {
                     initTable()
                 }
             })
-
             layer.close(index)
         })
     })
